aca615ac541cffe38a903186a6f8644e5a9d30c6
IA1phaI <pavel.2014.play@gmail.com>
2024-12-08 16:24:24 +0300

    docs(Vec2Math): add docs

--- lib/src/main/java/com/github/ia1phai/linear_algebra/vec/Vec2Math.java
+++ lib/src/main/java/com/github/ia1phai/linear_algebra/vec/Vec2Math.java
@@ -3,18 +3,39 @@ package com.github.ia1phai.linear_algebra.vec;
 import com.github.ia1phai.linear_algebra.Validator;
 
 /**
- * UncheckedVectorOperations
+ * Class with static functions for arbitrary vectors.
  */
 public class Vec2Math {
 
+    /**
+     * Calculates square of vector length.
+     * <p>
+     * You can use it if you need fast comparison.
+     * 
+     * @param v vector of size 2 for square length calculation
+     * @return square length of given vector
+     */
     public static float len2(final Vector2 v) {
         return v.x() * v.x() + v.y() * v.y();
     }
 
+    /**
+     * Calculates length of vector.
+     * 
+     * @param v vector 2 for length calculation
+     * @return Length of given vector
+     */
     public static float len(final Vector2 v) {
         return (float) Math.sqrt(len2(v));
     }
 
+    /**
+     * Multiplies the components of vector by a scalar value.
+     *
+     * @param v          vector of size 2 for multiplication
+     * @param multiplier scalar value
+     * @return given vector with multiplied components
+     */
     public static Vector2 mult(final Vector2 v, final float multiplier) {
         v.setX(v.x() * multiplier);
         v.setY(v.y() * multiplier);
@@ -22,10 +43,27 @@ public class Vec2Math {
         return v;
     }
 
+    /**
+     * Copies given vector and multiplies its components by a scalar.
+     * value.
+     * <p>
+     * Given vector doesn't change.
+     *
+     * @param v          vector of size 2 for multiplication
+     * @param multiplier scalar value
+     * @return new vector with multiplied components of given
+     */
     public static Vector2 multiplied(final Vector2 v, final float multiplier) {
         return mult(new Vec2(v), multiplier);
     }
 
+    /**
+     * Divides the components of vector by a scalar value.
+     * 
+     * @param v       vector of size 2 for division
+     * @param divisor scalar value
+     * @return given vector with divided components
+     */
     public static Vector2 divide(final Vector2 v, final float divisor) {
         Validator.validateDivisor(divisor);
 
@@ -35,18 +73,50 @@ public class Vec2Math {
         return v;
     }
 
+    /**
+     * Copies given vector and divides its the components of vector by a scalar
+     * value.
+     * <p>
+     * Given vector doesn't change.
+     *
+     * @param v       vector of size 2 for division
+     * @param divisor scalar value
+     * @return new vector with divided components
+     */
     public static Vector2 divided(final Vector2 v, final float divisor) {
         return divide(new Vec2(v), divisor);
     }
 
+    /**
+     * Normalizes given vector (divide each component by vector length)
+     * 
+     * @param v vector of size 2 to be normalized
+     * @return given vector with normalized components
+     */
     public static Vector2 normalize(final Vector2 v) {
         return divide(v, len(v));
     }
 
+    /**
+     * Copies and normalizes given vector (divide each component by vector length)
+     * <p>
+     * Given vector doesn't change.
+     * 
+     * @param v vector of size 2 to be normalized
+     * @return given vector with normalized components
+     */
     public static Vector2 normalized(final Vector2 v) {
         return divided(v, len(v));
     }
 
+    /**
+     * Adds the {@code addendum} vector components to the {@code target} vector
+     * components.
+     *
+     * @param target   vector of size 2 to be added
+     * @param addendum vector of size 2 to add
+     * @return {@code target} vector increased by {@code addendum} vector
+     */
     public static Vector2 add(final Vector2 target, final Vector2 addendum) {
         target.setX(target.x() + addendum.x());
         target.setY(target.y() + addendum.y());
@@ -54,10 +124,28 @@ public class Vec2Math {
         return target;
     }
 
+    /**
+     * Sums up the {@code addendum} vector and the {@code target} vector.
+     * <p>
+     * Given {@code target} vector doesn't change.
+     *
+     * @param target   vector of size 2 to be added
+     * @param addendum vector of size 2 to add
+     * @return new vector with sum of components of {@code target} vector and
+     *         {@code addendum} vector
+     */
     public static Vector2 added(final Vector2 target, final Vector2 addendum) {
         return add(new Vec2(target), addendum);
     }
 
+    /**
+     * Subtracts the {@code subtrahend} vector components from the {@code target}
+     * vector components.
+     * 
+     * @param target     vector of size 2 to be subtracted
+     * @param subtrahend vector of size 2 to subtract
+     * @return {@code target} vector subtracted by {@code subtrahend} vector
+     */
     public static Vector2 sub(final Vector2 target, final Vector2 subtrahend) {
         target.setX(target.x() - subtrahend.x());
         target.setY(target.y() - subtrahend.y());
@@ -65,26 +153,70 @@ public class Vec2Math {
         return target;
     }
 
+    /**
+     * Subtracts the {@code subtrahend} vector components from the {@code target}
+     * vector components.
+     * <p>
+     * Given {@code target} vector doesn't change.
+     * 
+     * @param target     vector of size 2 to be subtracted
+     * @param subtrahend vector of size 2 to subtract
+     * @return {@code target} vector subtracted by {@code subtrahend} vector
+     */
     public static Vector2 subtracted(final Vector2 target, final Vector2 subtrahend) {
         return sub(new Vec2(target), subtrahend);
     }
 
+    /**
+     * Calculates dot product (scalar product) of given vectors.
+     *
+     * @param v1 first vector of size 2
+     * @param v2 second vector of size 2
+     * @return dot (scalar) product of given vectors
+     */
     public static float dot(final Vector2 v1, final Vector2 v2) {
         return v1.x() * v2.x() + v1.y() * v2.y();
     }
 
+    /**
+     * Returns {@code true} if vector components are approximately equals.
+     * 
+     * @param v1 first vector of size 2 for comparison
+     * @param v2 second vector of size 2 for comparison
+     * @return {@code true} if all components are approximately equals, and
+     *         {@code false} otherwise
+     */
     public static boolean equals(final Vector2 v1, final Vector2 v2) {
-        return v1.x() == v2.x() && v1.y() == v2.y();
+        return Validator.equals(v1.x(), v2.x()) && Validator.equals(v1.y(), v2.y());
     }
 
+    /**
+     * Returns {@code true} if vector components are approximately equals.
+     * 
+     * @param v1 first vector of size 2 for comparison
+     * @param v2 second vector of size 2 for comparison
+     * @return {@code true} if all components are approximately equals, and
+     *         {@code false} otherwise
+     */
     public static Vector3 toVec3(final Vector2 v) {
         return new Vec3(v.x(), v.y(), 1);
     }
 
+    /**
+     * Constructs new vector of size 2 with all 0 components.
+     * 
+     * @param size size of vector to be constructed
+     * @return new zero vector of given {@code size}
+     */
     public static Vector2 zeroVec() {
         return new Vec2();
     }
 
+    /**
+     * Constructs new vector of size 2 with all 1 components.
+     * 
+     * @return new unit vector of size 2
+     */
     public static Vector2 unitVec() {
         return new Vec2(1, 1);
     }
