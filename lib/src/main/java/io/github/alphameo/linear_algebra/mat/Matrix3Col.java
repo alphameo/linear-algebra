0a6a455500080353348c4b5faead25b731fc5050
shimeoki <shimeoki@gmail.com>
2024-12-16 12:11:32 +0300

    docs(mat): add since in javadoc of mat3 & stringer

--- lib/src/main/java/io/github/alphameo/linear_algebra/mat/Matrix3Col.java
+++ lib/src/main/java/io/github/alphameo/linear_algebra/mat/Matrix3Col.java
@@ -2,18 +2,26 @@ package io.github.alphameo.linear_algebra.mat;
 
 /**
  * Enum of matrix 3x3 columns for safe access.
+ *
+ * @since 1.0.0
  */
 public enum Matrix3Col {
     /**
      * Column index 0
+     *
+     * @since 1.0.0
      */
     C0,
     /**
      * Column index 1
+     *
+     * @since 1.0.0
      */
     C1,
     /**
      * Column index 2
+     *
+     * @since 1.0.0
      */
     C2;
 }
--- lib/src/main/java/io/github/alphameo/linear_algebra/mat/Matrix3Row.java
+++ lib/src/main/java/io/github/alphameo/linear_algebra/mat/Matrix3Row.java
@@ -2,18 +2,26 @@ package io.github.alphameo.linear_algebra.mat;
 
 /**
  * Enum of matrix 3x3 rows for safe access.
+ *
+ * @since 1.0.0
  */
 public enum Matrix3Row {
     /**
      * Row index 0
+     *
+     * @since 1.0.0
      */
     R0,
     /**
      * Row index 1
+     *
+     * @since 1.0.0
      */
     R1,
     /**
      * Row index 2
+     *
+     * @since 1.0.0
      */
     R2;
 }
