989754cab7cf4cd8c2158d78a09dc83984d3d5d5
shimeoki <shimeoki@gmail.com>
2024-12-16 11:35:59 +0300

    docs(linear_algebra): add since to javadoc

--- lib/src/main/java/io/github/alphameo/linear_algebra/Copyable.java
+++ lib/src/main/java/io/github/alphameo/linear_algebra/Copyable.java
@@ -4,6 +4,8 @@ package io.github.alphameo.linear_algebra;
  * Interface for copyable entities.
  * 
  * @param <T> class for copying
+ *
+ * @since 1.0.0
  */
 public interface Copyable<T> {
 
@@ -11,6 +13,8 @@ public interface Copyable<T> {
      * Copies object values into new object of same class.
      * 
      * @return copy of object
+     *
+     * @since 1.0.0
      */
     T copy();
 }
