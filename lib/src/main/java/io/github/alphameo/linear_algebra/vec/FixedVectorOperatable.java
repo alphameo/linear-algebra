a4b31c5fb2e5eb912c317e81bb4b97ceac9ee8b0
alphameo <pavel.2014.play@gmail.com>
2025-01-13 16:59:22 +0300

    docs(vec): fix typos in fixed... interface

--- lib/src/main/java/io/github/alphameo/linear_algebra/vec/FixedVectorOperatable.java
+++ lib/src/main/java/io/github/alphameo/linear_algebra/vec/FixedVectorOperatable.java
@@ -7,7 +7,7 @@ package io.github.alphameo.linear_algebra.vec;
  * {@code V} is a vector class/interface which should implements this
  * operations
  *
- * {@code M} corresponding square matrix class
+ * {@code M} corresponding square matrix class/interface
  *
  * @since 3.0.0
  */
